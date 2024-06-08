68d05d6987b941c11686fe5b206247a3f87fba74
DemnArctis <demnarctis@gmail.com>
2024-06-08 16:09:30 +0300

    Move the routing module into the app module.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../routing/routing.module';
+import { AppRoutingModule } from './routing/routing.module';
 import { AppComponent } from './components/app/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
