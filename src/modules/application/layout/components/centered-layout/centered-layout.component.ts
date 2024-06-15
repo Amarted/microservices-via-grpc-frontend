b3270489fc2d83c529e87ba143ce4d90382488c3
DemnArctis <demnarctis@gmail.com>
2024-06-15 16:39:05 +0300

    Craeate centered layout.

---
+++ src/modules/application/layout/components/centered-layout/centered-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-centered-layout',
+  templateUrl: './centered-layout.component.html',
+  styleUrl: './centered-layout.component.sass',
+})
+export class CenteredLayoutComponent {
+
+}
--- src/modules/application/layout/layout.module.ts
+++ src/modules/application/layout/layout.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CenteredLayoutComponent } from './components/centered-layout/centered-layout.component';
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
     SidebarComponent,
     NavbarComponent,
+    CenteredLayoutComponent,
   ],
   imports: [
     CommonModule,
   ], exports: [
     MainLayoutComponent,
+    CenteredLayoutComponent,
   ],
 })
 export class LayoutModule { }
