094cb77fc01a1b9d1f197a2868533aa3821153ee
DemnArctis <demnarctis@gmail.com>
2024-06-16 17:41:47 +0300

    Add logout in the sidebar.

--- src/modules/application/layout/components/sidebar/sidebar.component.ts
+++ src/modules/application/layout/components/sidebar/sidebar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AccessService } from '../../../../auth/services/AccessService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.sass'
+  styleUrl: './sidebar.component.sass',
 })
 export class SidebarComponent {
+  public constructor(
+    private accessService: AccessService,
+    private router: Router,
+  ) { }
 
+  public async logout(): Promise<void> {
+    this.accessService.unauthorize();
+    await this.router.navigate([
+      '',
+    ]);
+  }
 }
